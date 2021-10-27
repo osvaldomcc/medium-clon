235f16508cbcf394685d1494b820a5ac8ad7f5b2
osvaldomcc <ocastilloc152@gmail.com>
2021-10-27 23:21:06 +0100

    Create Modules and config Lazy Load

---
+++ src/app/restricted/restricted.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RestrictedRoutingModule } from './restricted-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RestrictedRoutingModule
+  ]
+})
+export class RestrictedModule { }
